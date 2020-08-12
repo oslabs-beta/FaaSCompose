f93a340dd261392720ffc54b5f850d3e5138a316
jpascas <jpascas@gmail.com>
2020-08-11 21:52:01 -0500

    remove themeprovider from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,9 @@ import NextLink from 'next/link';
 import MuiLink from '@material-ui/core/Link';
 import { Button } from '@material-ui/core';
 import theme from '../components/Paperbase';
-import {
-  ThemeProvider,
-} from '@material-ui/core/styles';
 
 const Home = (): JSX.Element => {
   return (
-    <ThemeProvider theme={theme}>
     <div className="App">
       <Head>
         <title>FaaSCompose</title>
@@ -18,8 +14,7 @@ const Home = (): JSX.Element => {
       </Head>
       <BasicFlow />
       <Button color="primary">Hello World</Button>
-    </div>
-    </ThemeProvider>
+    </div>    
   );
 };
 
