f6ad96aab137a33baf2b850e62b5f7b7a6117170
Veronica Stork <zealous.anemone@gmail.com>
2020-08-15 15:02:08 -0400

    function editor saves function, random ID, name, and description to local storage

--- components/FunctionInventory.tsx
+++ components/FunctionInventory.tsx
@@ -1,17 +1,35 @@
-import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
+import {
+  Card,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Tooltip,
+  OverlayTrigger,
+} from 'react-bootstrap';
 import functions from '../data/functions.json';
-// import { useState } from 'react';
+import { useEffect } from 'react';
 
 const FunctionInventory = () => {
+  useEffect(() => {
+    window.localStorage.setItem('function', 'cat test');
+  });
+
   let funcs = [];
   functions.map((func) => {
-    funcs.push(<ListGroupItem>{func.name}</ListGroupItem>);
+    funcs.push(
+      <OverlayTrigger
+        placement="right"
+        overlay={<Tooltip id={func.id}>{func.description}</Tooltip>}
+      >
+        <ListGroupItem>{func.name}</ListGroupItem>
+      </OverlayTrigger>
+    );
   });
   return (
     <div>
       <Card>
         <Card.Body>
-          <Card.Title>Card Function Inventory</Card.Title>
+          <Card.Title>Cloud Function Inventory</Card.Title>
           <ListGroup className="list-group-flush">{funcs}</ListGroup>
           <Button variant="primary">New Function</Button>
         </Card.Body>
--- pages/func-editor.tsx
+++ pages/func-editor.tsx
@@ -1,16 +1,56 @@
+import { useRef, useState } from 'react';
+import { Container, Button, Form } from 'react-bootstrap';
 import Editor from '@monaco-editor/react';
-import { Container } from 'react-bootstrap';
 
 const FuncEditor = (): JSX.Element => {
+  const [isEditorReady, setIsEditorReady] = useState(false);
+  const valueGetter = useRef();
+
+  const handleEditorDidMount = (_valueGetter) => {
+    setIsEditorReady(true);
+    valueGetter.current = _valueGetter;
+  };
+
+  const addFunc = () => {
+    const id = String(Math.floor(Math.random() * 1000));
+    const name = (document.getElementById('name') as HTMLTextAreaElement).value;
+    const description = (document.getElementById(
+      'description'
+    ) as HTMLTextAreaElement).value;
+    const newFunc = valueGetter.current();
+    const jsonFunc = JSON.stringify({
+      id: id,
+      name: name,
+      description: description,
+      definition: newFunc,
+    });
+    window.localStorage.setItem(id, jsonFunc);
+  };
+
   return (
     <Container fluid>
       <Editor
-        height="50vh"
-        width="90vw"
+        height="300px"
+        width="450px"
         language="javascript"
         value={'// write your function here'}
         theme="dark"
+        editorDidMount={handleEditorDidMount}
       />
+      <Form>
+        <Form.Label>Function Name:</Form.Label>
+        <Form.Control type="text" placeholder="Name" id="name"></Form.Control>
+        <Form.Label>Function Description:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Description"
+          id="description"
+        ></Form.Control>
+
+        <Button variant="primary" onClick={addFunc}>
+          Add Function
+        </Button>
+      </Form>
     </Container>
   );
 };
