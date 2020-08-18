b5d07f0f61cef9ea4a1d864413fc639064fe97a6
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2020-08-17 21:37:51 -0400

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,45 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import BasicFlow from '../components/FlowChart';
-import {Button, Container, Row, Col, Card} from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+// import Execution from '../components/Execution/Execution';
 
 const Home = (): JSX.Element => {
   return (
     <div className="App">
       <Container fluid>
         <Row>
-          <Col xs={2} md={2} lg={2} style={{ background: '#333' }} >
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <hr />
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
-           <Button variant="primary" size="lg" variant="outline-light" block>action 1</Button>
+          <Col xs={2} md={2} lg={2} style={{ background: '#333' }}>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <hr />
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
+            <Button variant="primary outline-light" size="lg" block>
+              action 1
+            </Button>
           </Col>
           <Col xs={10} md={10} lg={10} style={{ background: '#bebebe' }}>
             <BasicFlow />
-            </Col>
+          </Col>
         </Row>
       </Container>
-      
+      {/* <Execution /> */}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
