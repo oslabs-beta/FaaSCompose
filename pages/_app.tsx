4b4d4108c567e07358912b0700a4fa0288c6994f
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2020-08-17 21:28:03 -0400

    Converts to tsx and adds types to _app

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import type { AppProps /*, AppContext */ } from 'next/app';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default MyApp;
