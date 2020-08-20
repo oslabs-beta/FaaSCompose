5859d2388fec53ac881cd86e82c8a7dded0f3746
jpascas <jpascas@gmail.com>
2020-08-19 22:04:58 -0500

    adding the login and deploy step in the UI

--- components/Execution/Execution.tsx
+++ components/Execution/Execution.tsx
@@ -2,12 +2,14 @@ import CloudProvidersDropdown from './CloudProvidersDropdown';
 import PlatformSpecific from './PlatformSpecific';
 import UserInput from './UserInput';
 import CompositionResult from './CompositionResult';
+import LoginDeploy from './LoginDeploy';
 
 const Execution = (props): JSX.Element => (
   <>
     <h3>Execution</h3>
     <CloudProvidersDropdown compositionName = { props.compositionName } />
     <PlatformSpecific />
+    <LoginDeploy compositionName = { props.compositionName }/>
     <UserInput compositionName = { props.compositionName }/>
     <CompositionResult />
   </>
---
+++ components/Execution/LoginDeploy.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import JSONInput from 'react-json-editor-ajrm/index';
+import locale from 'react-json-editor-ajrm/locale/en';
+import { Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectUserInput,
+  setUserInput,
+  setCompositionOutput,
+} from '../../store/reducers/executionReducer';
+
+const LoginDeploy = (props): JSX.Element => {
+  const dispatch = useDispatch();
+  const userInput = useSelector(selectUserInput);
+  const [outputText, setOutputText]= useState('');
+  let inputFromForm: string;
+
+  const dispatchSetUserInput = (payload) => dispatch(setUserInput(payload));
+  const dispatchSetCompositionOutput = (payload) =>
+    dispatch(setCompositionOutput(payload));
+
+  const handleInputChange = (formInput) => {
+    inputFromForm = formInput.json;
+  };
+
+  const handleClick = async () => {    
+    try {
+      const resLogin = await fetch(`http://localhost:3000/api/ibm/login`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }        
+      });
+      const loginOutputText = await resLogin.text();  
+      const resDeploy = await fetch(`http://localhost:3000/api/ibm/deploy/${props.compositionName}`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }        
+      });
+      const deployOutputText = await resDeploy.text(); 
+      setOutputText(loginOutputText + '\n' + deployOutputText);
+    } catch (error) {      
+      // if (error) throw new Error('Error from UserInput', error);
+    }
+  };
+
+  return (
+    <>      
+      <div className="inline">        
+        <Button variant="outline-primary" onClick={handleClick}>
+          Login and Deploy
+        </Button>              
+      </div>
+      <div>{outputText}</div>
+    </>
+  );
+};
+
+export default LoginDeploy;
