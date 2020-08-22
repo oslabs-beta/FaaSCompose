6c100be396e99365bca05ce422cbcbb81c091edb
jpascas <jpascas@gmail.com>
2020-08-22 12:21:21 -0500

    fix for Cloud selection dropdown

--- components/Execution/CloudProvidersDropdown.tsx
+++ components/Execution/CloudProvidersDropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DropdownButton, Dropdown, Button } from 'react-bootstrap';
 import { setComposition } from '../../store/reducers/executionReducer';
 import { useDispatch } from 'react-redux';
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 const CloudProvidersDropdown = (props): JSX.Element => {
   const dispatch = useDispatch();
   const dispatchSetComposition = (payload) => dispatch(setComposition(payload));
-
+  const [provider,setProvider] = useState('Cloud Providers');
   const handleClick = async () => {
     try {
       const compositionResponse = await fetch(
@@ -22,11 +22,11 @@ const CloudProvidersDropdown = (props): JSX.Element => {
   return (
     <div className="inline">
       {/* <span className="value">{composition}</span> */}
-      <DropdownButton id="dropdown-basic-button" title="Cloud Providers">
-        <Dropdown.Item href="/">IBM</Dropdown.Item>
-        <Dropdown.Item href="/">Netlify</Dropdown.Item>
-        <Dropdown.Item href="/">AWS</Dropdown.Item>
-        <Dropdown.Item href="/">Azure</Dropdown.Item>
+      <DropdownButton id="dropdown-basic-button" title={provider} onSelect={(e) => setProvider(e)}>
+        <Dropdown.Item eventKey="IBM Cloud">IBM Cloud</Dropdown.Item>
+        <Dropdown.Item eventKey="Google Cloud">Google Cloud</Dropdown.Item>
+        <Dropdown.Item eventKey="AWS">AWS</Dropdown.Item>
+        <Dropdown.Item eventKey="Azure">Azure</Dropdown.Item>
       </DropdownButton>
       <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
       <Button variant="outline-primary" onClick={handleClick}>
